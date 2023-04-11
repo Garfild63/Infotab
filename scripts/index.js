348f47a3a443b3052df302e6d25c5064b5342998
Garfild63 <garfild63@list.ru>
2023-04-11 16:12:32 +0300

    Tab to 4 spaces

--- scripts/index.js
+++ scripts/index.js
@@ -10,30 +10,30 @@ rawFile.onreadystatechange = function() {
 rawFile.send(null);
 
 ymaps.ready(function () {
-	var myMap = new ymaps.Map('map', {
+    var myMap = new ymaps.Map('map', {
         center: [44.68, 37.83],
         zoom: 11
     }, {
         searchControlProvider: 'yandex#search'
     }),
-	createMarker = (el) => {
-		return new ymaps.Placemark([el.lat, el.lng], {
-			hintContent: el.text,
-			balloonContent: '<p><h4 align="center">' + el.text + '</h4></p><p>' + el.description + '</p><p>' + el.eng_description + '</p>' +
-			(el.old_photo1 ? '<p align="center"><img src="images/' + el.old_photo1 + '" alt="Старое фото 1" width="350px"/></p>' : '') +
-			(el.old_photo2 ? '<p align="center"><img src="images/' + el.old_photo2 + '" alt="Старое фото 2" width="350px"/></p>' : '') +
-			(el.old_photo3 ? '<p align="center"><img src="images/' + el.old_photo3 + '" alt="Старое фото 3" width="350px"/></p>' : '') +
-			(el.photo ? '<p align="center"><img src="images/' + el.photo + '" alt="Фото" width="350px"/></p>' : '')
-		}, {
-			iconLayout: 'default#image',
+    createMarker = (el) => {
+        return new ymaps.Placemark([el.lat, el.lng], {
+            hintContent: el.text,
+            balloonContent: '<p><h4 align="center">' + el.text + '</h4></p><p>' + el.description + '</p><p>' + el.eng_description + '</p>' +
+            (el.old_photo1 ? '<p align="center"><img src="images/' + el.old_photo1 + '" alt="Старое фото 1" width="350px"/></p>' : '') +
+            (el.old_photo2 ? '<p align="center"><img src="images/' + el.old_photo2 + '" alt="Старое фото 2" width="350px"/></p>' : '') +
+            (el.old_photo3 ? '<p align="center"><img src="images/' + el.old_photo3 + '" alt="Старое фото 3" width="350px"/></p>' : '') +
+            (el.photo ? '<p align="center"><img src="images/' + el.photo + '" alt="Фото" width="350px"/></p>' : '')
+        }, {
+            iconLayout: 'default#image',
             iconImageHref: 'images/Marker.png',
-			iconImageSize: [15, 45],
-			iconImageOffset: [-7, -22]
-		});
-	},
-	markers = data;
-	markers.forEach(el => {
-		const marker = createMarker(el);
-		myMap.geoObjects.add(marker);
-	});
+            iconImageSize: [15, 45],
+            iconImageOffset: [-7, -22]
+        });
+    },
+    markers = data;
+    markers.forEach(el => {
+        const marker = createMarker(el);
+        myMap.geoObjects.add(marker);
+    });
 });
